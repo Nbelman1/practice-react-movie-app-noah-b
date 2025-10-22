a76045e71cfed6e425ce18d0f252c196243e686d
Noah <nbelman1@gmail.com>
2025-10-21 20:57:49 -0400

    Adjusted broken property to fix component

--- react-movie-app/src/App.jsx
+++ react-movie-app/src/App.jsx
@@ -4,28 +4,31 @@ import MovieSelector from './MovieSelector'
 
 function App() {
 
+  // useState instantiation
   const [ currentGenre, setCurrentGenre ] = useState('default');
 
   return (
     <>
+      <h1>Welcome to Movie Selector!</h1>
+
       <div>
         <select id="genreSelector" name="genre">
-          <option value="default">Select a genre</option>
+          {/* select-a-genre triggers error message */}
+          <option value="select-a-genre">Select a genre</option>
           <option value="action">Action</option>
           <option value="comedy">Comedy</option>
           <option value="horror">Horror</option>
         </select>
-
-        <br></br>
-        <br></br>
-
+      </div>
+      
+      <div>
         <button name="Fetch Button" value="fetch-button" 
           onClick={()=> setCurrentGenre(document.querySelector('select').value)}>
           Fetch Movies!!
         </button>
 
+        {/* creates prop named "genre", so that's what we pass into MovieSelector.jsx */}
         <MovieSelector genre={currentGenre} />
-
       </div>
     </>
   )
